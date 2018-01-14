bc992f903779e8f8e3e82ad55c78eadf1a224b12
Jean Kossaifi <jean.kossaifi@gmail.com>
2018-01-14 23:25:04 +0000

    New article

--- theme/scripts/sidescroll.js
+++ theme/scripts/sidescroll.js
@@ -1,33 +1,39 @@
 var sidebar = document.getElementById("side-scroll");
-right = Number(0.07*document.documentElement.clientWidth);
-sidebar.style.right = right + "px";
+var shift = 30;
+
+var set_side_scroll_right = function() {
+	var left = Number(10/12*window.innerWidth + shift);
+	sidebar.style.left = left + "px";
+}
 
 var toggle_side_scroll = function() {
     current_height = window.pageYOffset;
     article_height = document.getElementById("article").offsetHeight;
-	client_height = document.documentElement.clientHeight;
-	max_height = Number(article_height - client_height);
-    if ((current_height > 200) && (current_height < max_height)) {
+	inner_height = window.innerHeight;
+	max_height = Number(article_height - inner_height);
+
+	if ( (article_height <= inner_height) || (current_height < 200) ) {
+        sidebar.classList.add('fade');
+        sidebar.style.position = "fixed";
+        sidebar.style.top = "20%";
+	}
+	else if (current_height < max_height) {
         sidebar.classList.remove('fade');
         sidebar.style.position = "fixed";
-        sidebar.style.top = "40%";
+        sidebar.style.top = "20%";
     }
-    else if ((current_height >= max_height - 0.6*client_height) && (article_height > client_height)) {
+    else {
         sidebar.classList.remove('fade');
 		if (sidebar.style.position == "fixed") {
 			sidebar.style.position = "absolute";
-			sidebar.style.top = current_height + 0.4*client_height + "px";
+			sidebar.style.top = current_height + 0.2*inner_height + "px";
 		}
 	}
-	else {
-        sidebar.classList.add('fade');
-        sidebar.style.position = "fixed";
-        sidebar.style.top = "40%";
-    }
 }
 
 // check height but not too often to avoid lag
 var timer=0;
+set_side_scroll_right();
 window.addEventListener('scroll', function(e) {
     //if(timer) { window.clearTimeout(timer); }
     toggle_side_scroll();
@@ -35,4 +41,5 @@ window.addEventListener('scroll', function(e) {
     }, 100);
 });
 
+window.onresize = set_side_scroll_right;
 
